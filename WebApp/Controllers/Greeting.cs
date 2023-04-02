0194d26b43d9db6dc673f1fd2931b9f77cc050de
Oleksandr Parkhomenko <ipzm222_pol@student.ztu.edu.ua>
2023-04-02 18:22:04 +0300

    Wiorking MVC

--- WebApp/Controllers/Greeting.cs
+++ WebApp/Controllers/Greeting.cs
@@ -1,20 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
 
-
 namespace WebApp.Controllers
 {
-    public class HomeController : Controller
+    public class Greeting : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-        [HttpPost]
         public IActionResult Index(User user)
         {
             return View(user);
         }
-
     }
 }
---
+++ WebApp/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApp.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
